f049066be4091e9776cea3ef44176e81406e0178
dcderidder <dcderidderr@gmail.com>
2025-07-08 22:33:56 +0200

    chore: initial project import

---
+++ src/main/java/edu/inholland/OOP3_assignment_653284/Oop3Assignment653284Application.java
@@ -0,0 +1,13 @@
+package edu.inholland.OOP3_assignment_653284;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oop3Assignment653284Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oop3Assignment653284Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/inholland/OOP3_assignment_653284/Oop3Assignment653284ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.inholland.OOP3_assignment_653284;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oop3Assignment653284ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
